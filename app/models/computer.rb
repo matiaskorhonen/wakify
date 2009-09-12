0ecc547f6a9dfbff2821d0d151423408d1c1eda4
k33l0r <korhonen.matt@gmail.com>
2009-09-12 09:30:54 +0300

    Added validations to the computer model

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,3 +1,8 @@
 class Computer < ActiveRecord::Base
   belongs_to :user
+  
+  validates_presence_of :name, :host, :mac
+  validates_format_of :mac ,
+                     :with =>  /^(\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2})?$/ ,
+                     :message => "Invalid"
 end
