74f24eed8bee07a95002b6b3c7fec608db0145b1
k33l0r <korhonen.matt@gmail.com>
2009-09-12 10:32:38 +0300

    Formatted Computer validations

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -2,9 +2,9 @@ class Computer < ActiveRecord::Base
   belongs_to :user
   
   validates_presence_of :name, :host, :mac
-  validates_format_of :mac,
-                     :with =>  /^(\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2})?$/ ,
-                     :message => "invalid"
+  validates_format_of   :mac,
+                        :with =>  /^(\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2})?$/ ,
+                        :message => "invalid"
   validates_format_of   :broadcast_ip,
                         :with => /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})?$/,
                         :message => "invalid"
