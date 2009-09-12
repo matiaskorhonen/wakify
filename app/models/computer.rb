a80128fa9a49e0d3da101b00435f0dbd38deb613
k33l0r <korhonen.matt@gmail.com>
2009-09-12 09:47:19 +0300

    Added more validations

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -2,7 +2,10 @@ class Computer < ActiveRecord::Base
   belongs_to :user
   
   validates_presence_of :name, :host, :mac
-  validates_format_of :mac ,
+  validates_format_of :mac,
                      :with =>  /^(\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2})?$/ ,
-                     :message => "Invalid"
+                     :message => "invalid"
+  validates_format_of   :broadcast_ip,
+                        :with => /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})?$/,
+                        :message => "invalid"
 end
