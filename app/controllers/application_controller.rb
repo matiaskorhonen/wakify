6ddfc048cda34ed540f1ac075f0f21bc04d361dd
k33l0r <korhonen.matt@gmail.com>
2009-09-13 08:51:20 +0300

    Moved methods to their proper places.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,23 +5,9 @@ class ApplicationController < ActionController::Base
   include Authentication
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  require 'wakeonlan'
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-  
-  def quickwake(mac, host)
-    wol = WakeOnLan.new
-    return wol.wake(mac, "255.255.255.255", host)
-  end
-  
-  def system_ping(host = "127.0.0.1", count = 3, command = "ping")
-    return `#{command} #{host} -c #{count}`
-  end
-  
-  def system_wol(host = "127.0.0.1", mac = "", port = 9, command = "wakeonlan")
-    return `#{command} -p #{port} -i #{host} #{mac}`
-  end
 
   def valid_hostname?(address)
     address.to_s.chars.each { |character| return false unless ALLOWED_CHARS.include?(character) }
--- app/controllers/wakeonlan_controller.rb
+++ app/controllers/wakeonlan_controller.rb
@@ -1,4 +1,5 @@
 class WakeonlanController < ApplicationController
+  require 'wakeonlan'
   def index
   end
 
@@ -9,4 +10,18 @@ class WakeonlanController < ApplicationController
     @result = quickwake(@mac, @host_to_wake)
   end
 
+protected
+
+  def quickwake(mac, host)
+    wol = WakeOnLan.new
+    return wol.wake(mac, "255.255.255.255", host)
+  end
+  
+  def system_ping(host = "127.0.0.1", count = 3, command = "ping")
+    return `#{command} #{host} -c #{count}`
+  end
+  
+  def system_wol(host = "127.0.0.1", mac = "", port = 9, command = "wakeonlan")
+    return `#{command} -p #{port} -i #{host} #{mac}`
+  end
 end
