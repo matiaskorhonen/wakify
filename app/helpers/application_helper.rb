9f2fc506ad8f07dc52746d0cb23ec225ce48e728
k33l0r <korhonen.matt@gmail.com>
2009-09-16 09:58:34 +0300

    Added basic navigation. in_progress #7

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   include Authorization
+  
+  def navigation(*controllers)
+    xhtml = "<ul>\n"
+    
+    controllers.each do |controller|
+      xhtml << "\t<li>" + link_to(controller.capitalize, "/#{controller}") + "</li>\n"
+    end
+    
+    xhtml << "</ul>"
+  end
 end
