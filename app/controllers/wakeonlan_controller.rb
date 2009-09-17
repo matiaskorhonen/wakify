775a64e8514861bcd532f395cae350e86231755d
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-17 23:57:30 +0300

    Now sending TCP magic packaets only. closes #5

--- app/controllers/wakeonlan_controller.rb
+++ app/controllers/wakeonlan_controller.rb
@@ -4,36 +4,36 @@ class WakeonlanController < ApplicationController
   def index
   end
 
-  def wake
-    if params[:id].nil?
-      @mac = params[:mac]
-      @host_to_wake = params[:host_to_wake]
-      @result = quickwake(@mac, @host_to_wake, true)
+  def quickwake
+    @mac = params[:mac]
+    @host_to_wake = params[:host_to_wake]
+    port = params[:port].to_i ||= 9
+    
+    if @mac.empty? || @host_to_wake.empty?
+      flash[:error] = "You really do need a MAC and host address"
+      redirect_to :action => "index"
+    elsif !valid_hostname?(@host_to_wake)
+      flash[:error] = "You should enter a valid hostname or IP address"
+      redirect_to :action => "index"
     else
-      c = Computer.find(params[:id])
-      if c.access_allowed?(current_user)
-        @mac = c.mac
-        @host_to_wake = c.host
-        @result = quickwake(@mac, @host_to_wake, true)
-      else
-        flash[:error] = "Something went wrong, it seems that you are not allowed to wake that host!"
-        redirect_to :action => "index", :controller => "computers"
-      end
+      @result = wakehost(@mac, @host_to_wake, port)
     end
+  end
+  
+  def wake_computer
+    c = Computer.find(params[:id])
+    if c.access_allowed?(current_user)
+      flash[:notice] = wakehost(c.mac, c.host)
+    else
+      flash[:error] = "Something went wrong, it seems that you are not allowed to wake that host!"
+    end
+    
+    redirect_to :action => "index", :controller => "computers"
   end
 
 protected
 
-  def quickwake(mac, host, native="false")
-    if native
-      return system_wol(host, mac)
-    else
-      wol = WakeOnLan.new
-      return wol.wake(mac, "255.255.255.255", host)
-    end
-  end
-  
-  def system_wol(host = "127.0.0.1", mac = "", port = 9, command = "wakeonlan")
+  def wakehost(mac, host, port=APP_CONFIG[:wol_port], command = APP_CONFIG[:wol_command])
     return `#{command} -p #{port} -i #{host} #{mac}`
   end
 end
