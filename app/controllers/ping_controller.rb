d0fa4252c75141299f95bdac707d8b6cbdd42604
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-18 00:00:25 +0300

    Rewrote a lot of the controller. refs #6

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,34 +1,35 @@
 class PingController < ApplicationController
   def index
   end
-
-  def query
-    id = params[:id]
+  
+  def quickping
     @host_to_ping = params[:host_to_ping]
-    
-    if !id.nil? && @host_to_ping.nil?
-      c = Computer.find(id)
-      
-      if c.access_allowed?(current_user) && !c.nil?
-        @host_to_ping = c.host
-      else
-        flash[:error] = "Something went wrong, it seems that you are not allowed to ping that host!"
-        redirect_to :action => "index", :controller => "computers"
-      end
-    end
     
     if valid_hostname?(@host_to_ping) && !@host_to_ping.nil?
       @verbose = system_ping(@host_to_ping)
       p = Net::Ping::External.new(@host_to_ping)
       @result = p.ping
     else
-      @verbose = "You entered an invalid hostname"
+      flash[:error] = "You entered an invalid hostname."
+      redirect_to :action => "index"
     end
+  end
+  
+  def ping_computer
+    c = Computer.find(params[:id])
+    if c.access_allowed?(current_user)
+      status = Net::Ping::External.new(c.host) ? "up" : "down"
+      flash[:notice] = "<p>The host is #{status}.</p><pre>#{system_ping(c.host)}</pre>"
+    else
+      flash[:error] = "Something went wrong, it seems that you are not allowed to ping that host!"
+    end
+    
+    redirect_to :action => "index", :controller => "computers"
   end
   
 protected
 
-  def system_ping(host = "127.0.0.1", count = 3, command = "ping")
+  def system_ping(host = "127.0.0.1", count = 3, command = APP_CONFIG[:ping_command])
     return `#{command} #{host} -c #{count}`
   end
 end
