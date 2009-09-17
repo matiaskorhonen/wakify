ab9546115da4ec8615ce1114174ed04ae5f551ef
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-17 16:25:15 +0300

    Tried to clean up some code

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -3,21 +3,23 @@ class PingController < ApplicationController
   end
 
   def query
-    if params[:id].nil?
-      @host = params[:host_to_ping]
-    else
-      c = Computer.find(params[:id])
-      if c.access_allowed?(current_user)
-        @host = c.host
+    id = params[:id]
+    @host_to_ping = params[:host_to_ping]
+    
+    if !id.nil? && @host_to_ping.nil?
+      c = Computer.find(id)
+      
+      if c.access_allowed?(current_user) && !c.nil?
+        @host_to_ping = c.host
       else
         flash[:error] = "Something went wrong, it seems that you are not allowed to ping that host!"
         redirect_to :action => "index", :controller => "computers"
       end
     end
     
-    if valid_hostname?(@host) && !@host.nil?
-      @verbose = system_ping(@host)
-      p = Net::Ping::External.new(@host)
+    if valid_hostname?(@host_to_ping) && !@host_to_ping.nil?
+      @verbose = system_ping(@host_to_ping)
+      p = Net::Ping::External.new(@host_to_ping)
       @result = p.ping
     else
       @verbose = "You entered an invalid hostname"
