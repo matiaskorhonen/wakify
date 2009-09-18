945f0a7fc5ad2ba58cf8fb7474197963413e6128
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-18 23:02:50 +0300

    Routes for my earlier commits

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :pages
+
   map.signup 'signup', :controller => 'users', :action => 'new'
   map.logout 'logout', :controller => 'sessions', :action => 'destroy'
   map.login 'login', :controller => 'sessions', :action => 'new'
+  map.static 'static/:permalink', :controller => 'pages', :action => 'show'
+  map.account 'account', :controller => 'users', :action => 'edit'
   
   map.resources :sessions
   map.resources :users
   map.resources :computers
   map.resources :wakeonlan
+  map.resources :pages
 
   # The priority is based upon order of creation: first created -> highest priority.
 
