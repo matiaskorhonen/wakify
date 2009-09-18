bdcc65182e6c9ef031aedf2c954c5857d18df7e2
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-18 23:01:28 +0300

    Editable user accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :login_required, :except => [:new, :create]
   def new
     @user = User.new
+  end
+  
+  def edit
+    @user = current_user
   end
   
   def create
@@ -11,6 +16,24 @@ class UsersController < ApplicationController
       redirect_to root_url
     else
       render :action => 'new'
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.attributes = params[:user]
+    
+    respond_to do |format|
+      if @user.id == current_user.id
+        if @user.save
+          flash[:notice] = "User data updated"
+        else
+          flash[:error] = "Something went wrong"
+        end
+      else
+        flash[:error] = "Something went wrong"
+      end
+      format.html { render :action => "edit" }
     end
   end
 end
