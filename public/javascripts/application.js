80fcacb6f3bbf09e069b38dc82da5e1bdbec6d49
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-26 14:37:08 +0300

    Added Chroma Hash

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 $(document).ready(function() {
+  // Hide descriptions if JS is enabled
   $(".description").hide();
+  
+  /*  Add Chroma Hash to password fields
+      You can generate a new salt using script/console and the following command:
+      >> Digest::SHA1.hexdigest([Time.now, rand].join)
+  */
+  $("input:password:not(#captcha_attempt)").chromaHash({bars: 3, salt:"db0b3e7d4672a854def65e0dffbdf735bd195f10", minimum:6});
 });
