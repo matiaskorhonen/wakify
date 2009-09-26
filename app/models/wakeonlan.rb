d8a249245c2e530a274164a53047493d91575301
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-26 17:05:08 +0300

    Removed command line options from ruby wol.

--- app/models/wakeonlan.rb
+++ app/models/wakeonlan.rb
@@ -17,21 +17,3 @@ class WakeOnLan
 		count.times{ @socket.send(wol_magic, 0, ip, port) }
 	end
 end
-
-if $0 == __FILE__
-=begin
-Sample for WakeOnLan class.
-Use as: ruby wakeonlan.rb [broadcast_address] < data_text
-line format of data_text: ip-address  hostname  MAC-address
-Data sample: 10.20.30.40  wake-pc  100:110:120:130:140:150
-Note.  To check WOL packet, use tcpdump -x.
-=end
-
-	if ARGV.size>=1; broadcast=ARGV[0]; ARGV.clear; else broadcast=""; end
-	wol=WakeOnLan.new
-	while gets
-		ip,name,hw=$_.sub(/^\s+/,"").split(/\s+/)
-		wol.wake(hw, broadcast, ip)
-	end
-	wol.close
-end
