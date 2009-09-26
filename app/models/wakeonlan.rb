9020c47f18915d6b6deda94f0ff8c41b8d385ee8
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-26 16:51:44 +0300

    Moved Ruby WOl closer to the perl version.

--- app/models/wakeonlan.rb
+++ app/models/wakeonlan.rb
@@ -12,17 +12,9 @@ class WakeOnLan
 		@socket.setsockopt(Socket::SOL_SOCKET,Socket::SO_BROADCAST,1)
 	end;
 	def close; @socket.close; @socket=""; end
-	def wake(mac_addr, broadcast="", ip_addr="")
+	def wake(mac_addr, ip = "255.255.255.255", port = 9, count = 3)
 		wol_magic=(0xff.chr)*6+(mac_addr.split(/:/).pack("H*H*H*H*H*H*"))*16
-		if broadcast==""; # Set broadcast. Assume that standard IP-class.
-			ips=ip_addr.split(/\./);c=ips[0].to_i
-			if c<=127; ips[1]="255";end # class A:1--127
-			if c<=191; ips[2]="255";end # class B:128--191
-			if c<=223; ips[3]="255";end # class C:192--223
-			# class D:224--239 multicast
-			broadcast=ips.join(".")
-		end
-		3.times{ @socket.send(wol_magic,0,broadcast,"discard") }
+		count.times{ @socket.send(wol_magic, 0, ip, port) }
 	end
 end
 
