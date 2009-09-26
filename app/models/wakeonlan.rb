9b5ea7d9a28deed83e0f9248e595dae6d94b84e3
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-26 18:02:55 +0300

    Added documentation and stopped the wake command from sleeping forever if :interval is set to zero

--- app/models/wakeonlan.rb
+++ app/models/wakeonlan.rb
@@ -1,4 +1,5 @@
 # Copyright (C) 2000-2003  K.Kodama
+# Original: http://www.math.kobe-u.ac.jp/~kodama/tips-WakeOnLAN.html
 #
 # Modified by Matias Korhonen (26.09.2009: Removed command line options, changed to
 # send the magic packet straight to the IP, not the broadcast address)
@@ -8,19 +9,30 @@
 #
 require "socket"
 
+# Ruby version of the WakeOnLan command.  Originally written by K.Kodama.
 class WakeOnLan
   attr :socket
   
+  # Create a new instance. No arguments required.
   def initialize
     @socket=UDPSocket.open()
     @socket.setsockopt(Socket::SOL_SOCKET,Socket::SO_BROADCAST,1)
   end
   
+  # Close the socket opened by WakeOnLan initialization
   def close
     @socket.close
     @socket=""
   end
   
+  # Wake a host.
+  #
+  # == Options
+  # * <tt>:mac => "ff:ff:ff:ff:ff:ff"</tt> - Specify the destination MAC Address. Defaults to the broadcast MAC, "ff:ff:ff:ff:ff:ff"
+  # * <tt>:address => "255.255.255.255"</tt> - Specify the destination address.  Either a IP or hostname.  Defaults to "255.255.255.255"
+  # * <tt>:port => 9</tt> - The destination port. Defaults to the discard port, 9
+  # * <tt>:count => 1</tt> - How many time to send the MagicPacket.  Defaults to 1
+  # * <tt>:interval => 0.01</tt> - How many seconds to wait between sending packets. Defaults to 0.01
   def wake(options = {})
     mac = options[:mac] ||= "ff:ff:ff:ff:ff:ff"
     address = options[:address] ||= "255.255.255.255"
@@ -31,7 +43,7 @@ class WakeOnLan
     magicpacket = (0xff.chr)*6+(mac.split(/:/).pack("H*H*H*H*H*H*"))*16
     
     count.times {
-      sleep interval
+      sleep interval if interval > 0
       @socket.send(magicpacket, 0, address, port)
     }
   end
