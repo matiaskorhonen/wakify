977293a729a13cc26b786c52d60233ae76e38d90
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-28 14:25:01 +0300

    Fixed from email address

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -5,7 +5,7 @@ class Mailer < ActionMailer::Base
   # The welcome email sent to users when they join.
   def welcome_email(user)
     recipients    user.email
-    from          "#{APP_CONFIG[:sitename]} <notifications@example.com>"
+    from          "#{APP_CONFIG[:sitename]} <#{APP_CONFIG[:from_email]}>"
     subject       "Welcome to #{APP_CONFIG[:sitename]}"
     sent_on       Time.now
     content_type  "text/plain"
@@ -15,7 +15,7 @@ class Mailer < ActionMailer::Base
   # The password reset email sent to users when requested.
   def password_reset_email(user)
     recipients    user.email
-    from          "#{APP_CONFIG[:sitename]} <notifications@example.com>"
+    from          "#{APP_CONFIG[:sitename]} <#{APP_CONFIG[:from_email]}>"
     subject       "Password reset for #{APP_CONFIG[:sitename]}"
     sent_on       Time.now
     content_type  "text/plain"
