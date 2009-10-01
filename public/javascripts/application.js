6c7c01e5c60cac9ddf3ed428a8254fb5b5c56a14
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-10-01 11:57:07 +0300

    Added the begins of jQuery validation. refs #24

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,3 @@
-// Place your application-specific JavaScript functions and classes here
 $(document).ready(function() {
   // Hide descriptions if JS is enabled
   $(".description").hide();
@@ -13,11 +12,32 @@ $(document).ready(function() {
     return false;
   });
 
-  
   /*  Add Chroma Hash to password fields
       You can generate a new salt using script/console and the following command:
       >> Digest::SHA1.hexdigest([Time.now, rand].join)
   */
   $("input:password:not(#captcha_attempt)").chromaHash({bars: 3, salt:"db0b3e7d4672a854def65e0dffbdf735bd195f10", minimum:6});
+  
+  // Form Validations
+  $("#new_user").validate({
+    rules: {
+      "user[username]": {
+        required: true
+        },
+      "user[email]": {
+        required: true,
+        email: true
+      },
+      "user[password]": {
+        required: true,
+        minlength: 6
+      },
+      "user[password_confirmation]": {
+        required: true,
+        equalTo: "#user_password"
+      }
+    }
+  });
+
 });
 
