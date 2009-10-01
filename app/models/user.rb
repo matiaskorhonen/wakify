d2bf6f3c2f08df76f45af72dd12de6a86e5a6a43
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-10-01 16:38:07 +0300

    Added client side validations for the most important forms. closes #24

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   before_save :prepare_password
   before_create :add_activation_code
   
-  validates_presence_of :username
+  validates_presence_of :username, :minimum => 3
   validates_uniqueness_of :username, :email, :allow_blank => false, :case_sensitive => false
   validates_format_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => false, :message => "should only contain letters, numbers, or .-_@"
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -22,7 +22,9 @@ $(document).ready(function() {
   $("#new_user").validate({
     rules: {
       "user[username]": {
-        required: true
+        required: true,
+        minlength: 3,
+        username: true
         },
       "user[email]": {
         required: true,
@@ -38,6 +40,92 @@ $(document).ready(function() {
       }
     }
   });
+  
+  $(".edit_user").validate({
+    rules: {
+      "user[username]": {
+        required: true,
+        minlength: 3,
+        username: true
+        },
+      "user[email]": {
+        required: true,
+        email: true
+      },
+      "user[password]": {
+        required: false,
+        minlength: 6
+      },
+      "user[password_confirmation]": {
+        required: false,
+        equalTo: "#user_password"
+      }
+    }
+  });
 
+  $("#requests_form").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+      }
+    }
+  });
+  
+  $("#new_computer,.edit_computer").validate({
+    rules: {
+      "computer[name]": {
+        required: true
+      },
+      "computer[host]": {
+        required: true,
+        address: true
+      },
+      "computer[port]": {
+        required: true,
+        digits: true,
+        min: 0
+      },
+      "computer[mac]": {
+        required: true,
+        mac_address: true
+      },
+      "computer[description]": {
+        required: false
+      }
+    }
+  });
+  
+  $("#new_message").validate({
+    rules: {
+      "message[firstname]": {
+        required: true
+      },
+      "message[lastname]": {
+        required: true
+      },
+      "message[email]": {
+        required: true,
+        email: true
+      },
+      "message[subject]": {
+        required: true
+      },
+      "message[message]": {
+        required: true
+      }
+    }
+  });
 });
 
+jQuery.validator.addMethod("mac_address", function(value, element) { 
+  return this.optional(element) || /^(\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2})?$/.test(value); 
+}, "Please specify a valid MAC address");
+
+jQuery.validator.addMethod("address", function(value, element) { 
+  return this.optional(element) || /(^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$)|(^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$)/.test(value); 
+}, "Please specify a host (either an IP address or a hostname)");
+
+jQuery.validator.addMethod("username", function(value, element) { 
+  return this.optional(element) || /^[-\w\._@]+$/i.test(value); 
+}, "Should only contain letters, numbers, or .-_@");
