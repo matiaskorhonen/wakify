6222f73df787e288e18b36d3a07dd5237229d8bd
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-10-14 13:02:45 +0300

    Enabled caching of JS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,6 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# Enable to test caching in the development environment
+# config.action_controller.perform_caching = true
